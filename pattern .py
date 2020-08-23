8eb5f03822d12dd0f4810c43e401043a968a208b
bhagavanyalamarthi <61960124+bhagavanyalamarthi@users.noreply.github.com>
2020-08-23 17:23:37 +0530

    Create pattern .py
    print charecters pattern

---
+++ pattern .py
@@ -0,0 +1,10 @@
+a=65
+for i in range (5):
+    for j in range (5):
+        print(chr(i+65), end=" ")
+    print()
+#second model
+for i in range(5):
+    for j in range(65,70):
+        print(chr(j),end=" ")
+    print()
