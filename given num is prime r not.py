5e98f5915fe806e1b1e6ecce64071976930e0664
bhagavanyalamarthi <61960124+bhagavanyalamarthi@users.noreply.github.com>
2020-08-24 21:54:17 +0530

    Create given num is prime r not.py

---
+++ given num is prime r not.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number"))
+if n>1:
+for i in range(2,n+1):
+   if (n%i)==o:
+     print(n,is not a prime number)
+     break
+   else:
+     print(n,is a prime number)
+else:
+  print(n,not a prime number)
