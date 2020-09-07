8d468b69286bfc95e885e01d8044fcf8d52bcd9c
bhagavanyalamarthi <61960124+bhagavanyalamarthi@users.noreply.github.com>
2020-09-07 19:49:40 +0530

    Create sequence factorial.py
    for example we are taking ex= 10
    step 1 to find out given number factors  factors of 10is 1,2,5
    step2  add that num 1+2+5=10

---
+++ sequence factorial.py
@@ -0,0 +1,13 @@
+def xyz(n):
+    su=0
+    for i in range(1,n):
+        if n%2==0:
+            su+=1
+    return(s)
+n=int(input("enter a number")
+s=xyz(n)
+while s!=1 and s!=n:
+          n=s
+          s=xyz(s)
+          print(s)
+            
